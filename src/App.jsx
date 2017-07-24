7630750c3352bc214f5c1e414d741ce6f688857b
Eric Periou <eperiou@yahoo.com>
2017-07-23 23:50:45 -0500

    (feature) Add component lifecycle premount phase (cleanup) Clean up linter errors on Photogallery component`

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
+/* global fetch*/
 import React, { Component } from 'react';
 import './App.css';
-import PhotoGallery from './Components/PhotoGallery'
+import PhotoGallery from './Components/PhotoGallery';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      photos: [],
+    };
+  }
+  componentWillMount() {
+    /*
+    *  Prior to mounting component acquire photos from API
+    *  Sets the state of the component to have the photos be
+    *  the return from the API call
+    */
+  }
   render() {
     return (
       <div className="App">
-          <h1>Photogallery</h1>
-          <PhotoGallery />
+        <h1>Photogallery</h1>
+        <PhotoGallery photos={this.state.photos} />
       </div>
     );
   }
--- src/Components/PhotoGallery.jsx
+++ src/Components/PhotoGallery.jsx
@@ -1,40 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PhotoGallery = () => (
-      <article>
-        <h2>Photos!</h2>
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner" role="listbox">
-          {/* First Carousel item needs to be 'active' */}
-          <div className="carousel-item active">
-            <img className="d-block img-fluid" src="..." alt="First slide" />
-          </div>
-          <div className="carousel-item">
-            <img className="d-block img-fluid" src="..." alt="Second slide" />
-          </div>
+const PhotoGallery = ({ photos: {
+  name,
+  url,
+} }) => (
+  <article>
+    <h2>Photos!</h2>
+    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+      <div className="carousel-inner" role="listbox">
+        {/* First Carousel item needs to be 'active' */}
+        <div className="carousel-item active">
+          <img className="d-block img-fluid" src="..." alt="First slide" />
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="sr-only">Next</span>
-        </a>
+        <div className="carousel-item">
+          <img className="d-block img-fluid" src="..." alt="Second slide" />
+        </div>
       </div>
-      </article>
+      <a
+        className="carousel-control-prev"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="sr-only">Previous</span>
+      </a>
+      <a
+        className="carousel-control-next"
+        href="#carouselExampleControls"
+        role="button"
+        data-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="sr-only">Next</span>
+      </a>
+    </div>
+  </article>
 );
 
-// PhotoGallery.propTypes = {
-//   projectnumber: PropTypes.number.isRequired,
-//   project: PropTypes.shape({
-//     title: PropTypes.string.isRequired,
-//     captions: PropTypes.array.isRequired,
-//     explanation: PropTypes.string.isRequired,
-//     images: PropTypes.array.isRequired,
-//     link: PropTypes.string.isRequired,
-//   }).isRequired,
-// };
+PhotoGallery.propTypes = {
+  photos: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+};
+/*
+  Sample of incoming photo objects
+{
+  "uuid": "3f35e6dc-b0c6-4bf5-abe5-908626b649ab",
+  "name": "1024px-Teliris_VL_Modular.jpg",
+  "url": "https://sparkhire-dev.s3.amazonaws.com/company/e9aafdd9-d9f3-455e-b495-58d087415857/files/3f35e6dc-b0c6-4bf5-abe5-908626b649ab/3f35e6dc-b0c6-4bf5-abe5-908626b649ab.jpg",
+},
 
+*/
 export default PhotoGallery;
