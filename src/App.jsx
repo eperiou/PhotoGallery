fa6c12def9c599a459d6491bbbdbb27766204f04
Eric Periou <eperiou@yahoo.com>
2017-07-23 23:56:46 -0500

    (feature) add Axios library, made request for photos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 /* global fetch*/
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import PhotoGallery from './Components/PhotoGallery';
 
@@ -16,6 +17,13 @@ class App extends Component {
     *  Sets the state of the component to have the photos be
     *  the return from the API call
     */
+    axios('http://api.new-feature.sparkhire.com/v1.0/companies/e9aafdd9-d9f3-455e-b495-58d087415857/photos')
+      .then(({ data }) => {
+        this.setState({
+          photos: data,
+        });
+      })
+      .catch(err => console.error(err));
   }
   render() {
     return (
